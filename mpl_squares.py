565011f6cc365c0d83bd958e0c57ec4a00e6839a
Pedro Rodrigues <pedrolrodrigues3@gmail.com>
2023-11-28 11:02:17 -0300

    adjusting squares_plot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+input_values = range(1, 1001)
+squares = [x**2 for x in input_values]
 
 # The plt.style.use() method allows me to change the background color to the chart
 plt.style.use('seaborn-v0_8-darkgrid')
@@ -13,15 +13,14 @@ The variable ax represents a single plot in the figure and is the variable i'll 
 fig, ax = plt.subplots()
 
 # The scatter() method allows me to plot a series of points passing first the values of xlabel then ylabel
-ax.scatter(input_values, squares)
-
-# The plot() method will try to plot the data it's given in a meaningfull way.
-ax.plot(input_values, squares, linewidth=3)
+ax.scatter(input_values, squares, s=10)
 
 #Set chart title and label axes
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
+
+ax.axis([0, 1100, 0, 1100000])
 
 # Set the size of tick labels
 ax.tick_params(axis="both", labelsize=10)
